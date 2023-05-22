de8b579f3917bbd529ffb2d41965e5579181b26a
JM <jakub.matwis@gmail.com>
2023-05-21 18:40:57 -0700

    fix/SearchInput: width & position styling

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -20,10 +20,10 @@ export const SearchInput = () => {
     }
 
     return (
-        <form onSubmit={handleSearch} className="w-1/2">
+        <form onSubmit={handleSearch} className="w-full">
             <input
                 type="text"
-                className="w-full px-8 py-3 text-center text-gray-100 bg-gray-600 rounded-full sm:px-4 placeholder:text-gray-400"
+                className="block w-full max-w-sm p-2 mx-auto my-2 text-center text-gray-100 bg-gray-800 rounded-full placeholder:text-gray-400"
                 placeholder="What kind of ship are you looking for?"
                 onChange={handleQuery}
                 value={query}
