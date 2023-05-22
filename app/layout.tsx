0c718d36b6906d861f99a272ba0be501cbdd9804
JM <jakub.matwis@gmail.com>
2023-05-21 18:45:56 -0700

    feat: navigation added to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import '@/app/globals.css'
+import { FilmLink } from '@/components/FilmLink'
 import { SearchInput } from '@/components/SearchInput'
+import getAllFilms from '@/service/getAllFilms'
 import { store } from '@/store'
 import { setStarships } from '@/store/searchSlice'
 import { Starship } from '@/types/Starship'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Hangar Bay',
@@ -12,26 +15,37 @@ export const metadata = {
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  params: { id: string }
 }) {
   const res = await fetch("https://swapi.dev/api/starships/")
   const data = await res.json();
   const starships = data.results
   store.dispatch(setStarships(starships))
+
+  const films = await getAllFilms()
+
   return (
     <html lang="en">
       <body className='text-gray-100 bg-black'>
-        <header className='flex flex-col items-center w-full max-w-5xl gap-10 p-6 mx-auto'>
+        <header className='flex flex-col items-center w-full gap-2 p-6 mx-auto'>
           <h1 className='text-6xl font-bold'>Welcome to the
-            <span className='text-transparent bg-gradient-to-r from-blue-500 via-violet-500 to-red-700 bg-clip-text'>
+            <Link href="/" className='text-transparent bg-gradient-to-r from-blue-500 via-violet-500 to-red-700 bg-clip-text'>
               &nbsp;Hangar&nbsp;Bay
-            </span>
+            </Link>
           </h1>
           <h2 className='text-2xl'>
             Search for your favourite Star Wars starships!
           </h2>
-          <nav className='sticky top-0 flex justify-center w-full'>
+          <nav className='flex flex-col items-center w-full'>
             <SearchInput />
+            <div className="flex flex-wrap mt-3">
+              {
+                films
+                  .map((film, index) =>
+                    <FilmLink key={index} index={index} title={film.title} />)
+              }
+            </div>
           </nav>
         </header>
         <main className="flex flex-col items-center w-full border-t-2 border-gray-700">
