e7270edce5e93a75a8f06ae7e1281f6b28e73002
JM <jakub.matwis@gmail.com>
2023-05-21 18:46:15 -0700

    feat: search page created

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import getAllStarships from "@/service/getAllStarships";
 import { useSearchParams } from "next/navigation"
 
-export default function Search() {
+export default async function Search() {
 
     const search = useSearchParams();
     const searchQuery = search ? search.get("q") : null
-
+    const starships = await getAllStarships();
     return <div className="">Here is what we&apos;ve got for ya</div>
 }
