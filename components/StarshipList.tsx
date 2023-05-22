43bbcf796e8ecbdb4cc13df70ac1eddef497d838
JM <jakub.matwis@gmail.com>
2023-05-21 18:41:53 -0700

    fix/StarshipList: Link href fix to always refer to allStarships array

--- components/StarshipList.tsx
+++ components/StarshipList.tsx
@@ -1,3 +1,4 @@
+import getAllStarships from "@/service/getAllStarships";
 import { Starship } from "@/types/Starship";
 import { creditConvert } from "@/utils/creditConvert";
 import Link from "next/link";
@@ -7,21 +8,28 @@ interface Props {
     starships: Starship[]
 }
 
-export const StarshipList = ({ starships }: Props) => {
+export const StarshipList = async ({ starships }: Props) => {
+    const allStarships = await getAllStarships();
+
     return (
         <div className="w-full">
-            {starships.map((ship, index) =>
-                <div key={ship.url} className="flex items-center justify-between p-3 m-5 text-2xl bg-gray-900 border border-gray-700 rounded-lg">
-                    <h3 className="mr-auto text-2xl">
-                        {ship.name}
-                    </h3>
-                    <p>
-                        {creditConvert(ship.cost_in_credits)}
-                    </p>
-                    <Link href={`/ship/${index}`} className="ml-4">
-                        <BsFillArrowRightCircleFill />
-                    </Link>
-                </div>)
+            {
+                starships.map((ship) =>
+                    <div key={ship.url} className="flex items-center justify-between p-3 m-5 text-2xl bg-gray-900 border border-gray-700 rounded-lg">
+                        <h3 className="mr-auto text-2xl">
+                            {ship.name}
+                        </h3>
+                        <p>
+                            {creditConvert(ship.cost_in_credits)}
+                        </p>
+                        <Link href={`/ship/${allStarships.findIndex((element) => element.name === ship.name)}`} className="ml-4">
+                            <BsFillArrowRightCircleFill />
+                        </Link>
+                    </div>
+                )
+            }
+            {
+                starships.length === 0 && <p className="mt-6 text-xl text-center text-red-400">This hangar is empty!</p>
             }
         </div>
     )
