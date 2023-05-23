3f695f1887a2952b8e62324471fc3931a4ad3008
JM <jakub.matwis@gmail.com>
2023-05-22 17:18:42 -0700

    fix: changed creditConvert to recursive function, added new prefixes B & M

--- utils/creditConvert.ts
+++ utils/creditConvert.ts
@@ -1,4 +1,22 @@
-export const creditConvert = (credits: string): string => {
-  if (credits === "unknown") return "";
-  return Math.ceil(Number(credits) / 1000) + "K";
+export const creditConvert = (
+  credits: string,
+  divider = 1000000000
+): string => {
+  if (credits === "unknown") return "out of stock";
+  const creditsNumber = Number(credits);
+  const unitPrefixes = {
+    1000: "K",
+    1000000: "M",
+    1000000000: "B",
+  };
+
+  if (creditsNumber / divider > 1) {
+    return (
+      (creditsNumber / divider).toPrecision(4) +
+      unitPrefixes[divider as keyof typeof unitPrefixes] +
+      " ₹"
+    );
+  }
+  divider /= 1000;
+  return creditConvert(credits, divider);
 };
