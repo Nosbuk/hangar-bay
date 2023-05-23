93324802ae51c07a91534f458b1cc16498bf5bc5
JM <jakub.matwis@gmail.com>
2023-05-22 17:33:33 -0700

    fix: slug removed from Film object

--- service/getAllFilms.ts
+++ service/getAllFilms.ts
@@ -5,12 +5,8 @@ export default async function getAllFilms(): Promise<Film[]> {
   const res = await fetch("https://swapi.dev/api/films/");
   const data = await res.json();
   const { results } = data;
-  return results
-    .map((film: Film) =>
-      film.slug ? film : { ...film, slug: slugify(film.title) }
-    )
-    .sort(
-      (prev: Film, curr: Film) =>
-        Number(prev.episode_id) - Number(curr.episode_id)
-    );
+  return results.sort(
+    (prev: Film, curr: Film) =>
+      Number(prev.episode_id) - Number(curr.episode_id)
+  );
 }
