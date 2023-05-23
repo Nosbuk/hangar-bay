8a69cbc3f89ddf165e5e9d6e903e503ca52cd44d
JM <jakub.matwis@gmail.com>
2023-05-22 17:40:04 -0700

    fix: restyling for mobile

--- components/StarshipList.tsx
+++ components/StarshipList.tsx
@@ -15,7 +15,7 @@ export const StarshipList = ({ starships }: Props) => {
         <article className="flex flex-col items-center w-full mt-2">
             {
                 starships.map((ship) =>
-                    <section key={ship.url} >
+                    <section key={ship.url} className="flex items-center justify-center w-full">
                         <Link href={`/ship/${ship.slug}`} className="flex items-center justify-between w-full p-3 my-2 text-sm bg-gray-900 rounded-none sm:max-w-xl sm:rounded-lg sm:text-lg hover:bg-gray-800">
                             <h3 className="mr-auto">
                                 {ship.name}
