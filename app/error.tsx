9e02f2c42e43c1e5544217bb38e9c4b9edf15114
JM <jakub.matwis@gmail.com>
2023-05-22 17:10:15 -0700

    feat: error handling and component done

---
+++ app/error.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Link from "next/link";
+import { AiFillHome } from "react-icons/ai";
+import { FiRefreshCcw } from "react-icons/fi";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    return <div className="flex flex-col items-center gap-3">
+        <p className="mt-6 text-xl font-bold text-center text-red-400">{error.message}</p>
+        <Link href="/" className="flex items-center gap-2 underline"><AiFillHome />Home Page</Link>
+        <button className="flex items-center gap-2 underline" onClick={reset}><FiRefreshCcw />Try again</button>
+    </div>
+}
