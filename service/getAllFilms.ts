bacbf396472315e3429139d0716a1f43b53388ed
JM <jakub.matwis@gmail.com>
2023-05-22 17:22:45 -0700

    fix: removed slug from type / can use episode_id instead

--- service/getAllFilms.ts
+++ service/getAllFilms.ts
@@ -1,7 +1,16 @@
 import { Film } from "@/types/Film";
+import slugify from "slugify";
 
 export default async function getAllFilms(): Promise<Film[]> {
   const res = await fetch("https://swapi.dev/api/films/");
   const data = await res.json();
-  return data.results;
+  const { results } = data;
+  return results
+    .map((film: Film) =>
+      film.slug ? film : { ...film, slug: slugify(film.title) }
+    )
+    .sort(
+      (prev: Film, curr: Film) =>
+        Number(prev.episode_id) - Number(curr.episode_id)
+    );
 }
