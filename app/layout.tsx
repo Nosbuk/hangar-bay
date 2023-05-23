3f8a7f9795128405c89cf7fb864fd9570b3d4b71
JM <jakub.matwis@gmail.com>
2023-05-22 17:24:29 -0700

    feat: footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import '@/app/globals.css'
-import { FilmLink } from '@/components/FilmLink'
-import { SearchInput } from '@/components/SearchInput'
-import getAllFilms from '@/service/getAllFilms'
-import { store } from '@/store'
-import { setStarships } from '@/store/searchSlice'
-import { Starship } from '@/types/Starship'
-import Link from 'next/link'
+import "@/app/globals.css"
+import { FilmLinks } from "@/components/FilmLinks"
+import { SearchInput } from "@/components/SearchInput"
+import getAllFilms from "@/service/getAllFilms"
+import Link from "next/link"
 
 export const metadata = {
-  title: 'Hangar Bay',
-  description: 'Search and look up the best starships in the galaxy',
+  title: "Hangar Bay",
+  description: "Search and look up the best starships in the galaxy",
 }
 
 export default async function RootLayout({
@@ -18,39 +15,29 @@ export default async function RootLayout({
   children: React.ReactNode,
   params: { id: string }
 }) {
-  const res = await fetch("https://swapi.dev/api/starships/")
-  const data = await res.json();
-  const starships = data.results
-  store.dispatch(setStarships(starships))
 
   const films = await getAllFilms()
 
+  if (!films) throw new Error("Films list not found")
+
   return (
     <html lang="en">
-      <body className='text-gray-100 bg-black'>
-        <header className='flex flex-col items-center w-full gap-2 p-6 mx-auto'>
-          <h1 className='text-6xl font-bold'>Welcome to the
-            <Link href="/" className='text-transparent bg-gradient-to-r from-blue-500 via-violet-500 to-red-700 bg-clip-text'>
-              &nbsp;Hangar&nbsp;Bay
-            </Link>
-          </h1>
-          <h2 className='text-2xl'>
-            Search for your favourite Star Wars starships!
-          </h2>
-          <nav className='flex flex-col items-center w-full'>
+      <body className="text-gray-100 bg-black">
+        <header className="flex flex-col items-center w-full mx-auto">
+          <Link href="/" className="mt-12 text-5xl font-bold leading-normal text-transparent sm:mt-0 lg:text-7xl bg-gradient-to-r from-blue-500 via-violet-500 to-red-700 bg-clip-text">
+            <h1 className="leading-normal">Hangar&nbsp;Bay</h1>
+          </Link>
+          <nav className="flex flex-col items-center justify-center w-full">
             <SearchInput />
-            <div className="flex flex-wrap mt-3">
-              {
-                films
-                  .map((film, index) =>
-                    <FilmLink key={index} index={index} title={film.title} />)
-              }
-            </div>
+            <FilmLinks films={films} />
           </nav>
         </header>
-        <main className="flex flex-col items-center w-full border-t-2 border-gray-700">
+        <main className="flex flex-col items-center w-full mb-10 border-t-2 border-gray-700">
           {children}
         </main>
+        <footer className="fixed bottom-0 flex items-center justify-center w-full text-sm bg-gray-900 border-t-2 border-gray-700">
+          Hangar Bay by&nbsp;<span className="font-bold text-transparent bg-gradient-to-r from-blue-500 via-violet-500 to-red-700 bg-clip-text">Jakub Matwis</span>&nbsp;|&nbsp;Data from&nbsp;<a target="_blank" rel="external" href="https://swapi.dev/" className="text-yellow-500">SWAPI.dev</a>
+        </footer>
       </body>
     </html>
   )
