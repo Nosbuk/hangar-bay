915d031df0d024665dd27e44a8bfe2481d5afeb6
JM <jakub.matwis@gmail.com>
2023-05-22 17:23:39 -0700

    fix: error handling moved to error handler

--- app/film/[id]/page.tsx
+++ app/film/[id]/page.tsx
@@ -1,18 +1,18 @@
 import { StarshipList } from "@/components/StarshipList";
 import getAllFilms from "@/service/getAllFilms";
 import getAllStarships from "@/service/getAllStarships";
+import { PageProps } from "@/types/PageProps";
 
 export async function generateStaticParams() {
     const films = await getAllFilms()
 
-    return films.map((film, index) => ({ index }))
+    return films.map((film) => ({ id: film.episode_id.toString() }))
 }
 
-export default async function Film({ params }: { params: { id: string } }) {
-    const index = Number(params.id)
+export default async function Film({ params }: PageProps) {
     const films = await getAllFilms()
-    const film = films[index]
-
+    const film = films.find((film) => film.episode_id.toString() === params?.id)
+    if (!film) throw new Error("Film not found")
     const starships = await getAllStarships()
     const filteredStarships = starships.filter((ship) => {
         return film.starships.includes(ship.url)
